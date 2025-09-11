5749b592c4ab5d220d9989d11073f6528fa568ae
Alex Mota Brito Cezar de Oliveira <alexmbcdo@gmail.com>
2025-09-11 12:29:09 -0300

    rollback imports

--- impacta/src/br/com/impacta/Main.java
+++ impacta/src/br/com/impacta/Main.java
@@ -1,5 +1,11 @@
 package br.com.impacta;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Main extends Application {
 
     @Override
